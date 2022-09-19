671f4728c9afe43162b2399c1446b4f26f9dad73
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2022-09-19 20:09:58 -0300

    mobile finalizado

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from "react";
 import { StatusBar } from "react-native";
 import {
 	useFonts,
@@ -6,10 +7,15 @@ import {
 	Inter_700Bold,
 	Inter_900Black,
 } from "@expo-google-fonts/inter";
+import { Subscription } from "expo-modules-core";
 
 import { Routes } from "./src/routes";
 import { Background } from "./src/components/Background";
 import { Loading } from "./src/components/Loading";
+
+import "./src/services/notificationConfig";
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
+import * as Notifications from "expo-notifications";
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -18,6 +24,41 @@ export default function App() {
 		Inter_700Bold,
 		Inter_900Black,
 	});
+
+	const getNotificationListener = useRef<Subscription>();
+	const responseNotificationListener = useRef<Subscription>();
+
+	useEffect(() => {
+		getPushNotificationToken();
+	});
+
+	useEffect(() => {
+		getNotificationListener.current =
+			Notifications.addNotificationReceivedListener((notification) => {
+				const data = notification.request.content.data;
+				console.log(data);
+			});
+
+		responseNotificationListener.current =
+			Notifications.addNotificationResponseReceivedListener((response) => {
+				const data = response.notification.request.content.data;
+				console.log(data);
+			});
+
+		return () => {
+			if (
+				getNotificationListener.current &&
+				responseNotificationListener.current
+			) {
+				Notifications.removeNotificationSubscription(
+					getNotificationListener.current
+				);
+				Notifications.removeNotificationSubscription(
+					responseNotificationListener.current
+				);
+			}
+		};
+	}, []);
 
 	return (
 		<Background>
--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -11,9 +11,11 @@ import { styles } from "./styles";
 import logoImg from "../../assets/logo-nlw-esports.png";
 import { THEME } from "../../theme";
 import { DuoCard, DuoCardProps } from "../../components/DuoCard";
+import { DuoMatch } from "../../components/DuoMatch";
 
 export function Game() {
 	const [duos, setDuos] = useState<DuoCardProps[]>([]);
+	const [discordDuoSelected, setDiscordDuoSelected] = useState("");
 
 	const navigation = useNavigation();
 
@@ -22,6 +24,12 @@ export function Game() {
 
 	function handleGoBack() {
 		navigation.goBack();
+	}
+
+	async function getDiscordUser(adsId: string) {
+		fetch(`http://10.0.0.139:3333/ads/${adsId}/discord`)
+			.then((res) => res.json())
+			.then((data) => setDiscordDuoSelected(data.discord));
 	}
 
 	useEffect(() => {
@@ -57,7 +65,9 @@ export function Game() {
 			<FlatList
 				data={duos}
 				keyExtractor={(item) => item.id}
-				renderItem={({ item }) => <DuoCard data={item} onConnect={() => {}} />}
+				renderItem={({ item }) => (
+					<DuoCard data={item} onConnect={() => getDiscordUser(item.id)} />
+				)}
 				horizontal
 				style={styles.containerList}
 				contentContainerStyle={[
@@ -69,6 +79,12 @@ export function Game() {
 						Ainda não há anúncios publicados para este jogo.
 					</Text>
 				)}
+			/>
+
+			<DuoMatch
+				visible={discordDuoSelected.length > 0}
+				discord={discordDuoSelected}
+				onClose={() => setDiscordDuoSelected("")}
 			/>
 		</SafeAreaView>
 	);
